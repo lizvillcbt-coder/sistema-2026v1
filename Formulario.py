02f688b4649d0e10763a5c693443dff6ec686e2b
Alexander Villalobos Lazaro <150092781+AlexanderVL27@users.noreply.github.com>
2026-08-11 11:25:51 -0600

    Add files via upload

--- Formulario.py
+++ Formulario.py
@@ -408,11 +408,13 @@ OPCIONES_SEMESTRE_MOSTRAR = [
 ]
 
 MAP_MOSTRAR_A_VALOR = {
+    "1er Semestre (1er Año)": "1ER SEMESTRE",
     "3er Semestre (2do Año)": "3ER SEMESTRE",
     "5to Semestre (3er Año)": "5TO SEMESTRE",
 }
 
 MAP_VALOR_A_MOSTRAR = {
+    "1ER SEMESTRE": "1er Semestre (1er Año)",
     "3ER SEMESTRE": "3er Semestre (2do Año)",
     "5TO SEMESTRE": "5to Semestre (3er Año)",
 }
